29c08758efd9e46fc00af0be965c5376033b3628
UmaBalasubramanian <umabalu93@gmail.com>
2017-08-19 18:39:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "Hola!!";
 
-var marginLeft = 0;
+
 var img = document.getElementById('img');
+var marginLeft = 0;
 function moveRight(){
         marginLeft = marginLeft +1;
      img.style.marginLeft = marginLeft+"px";
