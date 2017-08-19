74d65ee21e535aa3538fa5c69e8d1dc1d697e757
UmaBalasubramanian <umabalu93@gmail.com>
2017-08-19 19:44:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,21 @@
 console.log('Loaded!');
 
-var counter =0;
 var btnCounter = document.getElementById('btnCounter');
 var spanCounter = document.getElementById('counter');
 
 btnCounter.onclick = function(){
-    counter = counter +1;
-    spanCounter.innerHTML = counter.toString();
+    //counter = counter +1;
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status == 200){
+              var  counter = request.responseText;
+              spanCounter.innerHTML = counter.toString();
+            }
+        }
+    }
+    request.open('GET', 'http://umabalu93.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
 };
