3abce03df04a5af1bda118ec01a5bded0eb26456
UmaBalasubramanian <umabalu93@gmail.com>
2017-08-19 20:10:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ btnSubmit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState == XMLHttpRequest.DONE){
             if(request.status == 200){
-                var names = ['name1','name2','name3'];
+                var names = JSON.parse(request.responseText);
                 var list = '';
                 for(var i=0; i<names.length;i++){
                     list += '<li>' + names[i] +'</li>';
