16fbcb66f6d4586443f3853f76f49ae53deb3997
UmaBalasubramanian <umabalu93@gmail.com>
2017-08-22 11:32:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 
-var btnSubmit = document.getElementById('btnSubmit');
+var btnLogin = document.getElementById('btnLogin');
+var btnRegister = document.getElementById('btnRegister');
 btnSubmit.onclick = function(){
     
     var request = new XMLHttpRequest();
@@ -29,4 +30,24 @@ btnSubmit.onclick = function(){
     request.setRequestHeader('Content-Type',' application/json');
     request.send(JSON.stringify({username: name, password: password}));
     
+}
+btnRegister.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.status == 200){
+            //Replace the Login div by the username
+            alert('User created successfully');
+        }
+        else if(request.status == 500){
+            alert('Some error occured!!');
+        }
+    }
+    var nameElement = document.getElementById('username');
+    var name = nameElement.value;
+    var passwordElement = document.getElementById('password');
+    var password = passwordElement.value;
+    console.log(name +" "+ password);
+    request.open('POST','http://umabalu93.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type',' application/json');
+    request.send(JSON.stringify({username: name, password: password})); 
 }
